befc84985aef569f872d56c5903011b2e321dd3f
ashrafdiab96 <53093337+ashrafdiab96@users.noreply.github.com>
2020-03-22 23:06:56 +0200

    Update handelReviewRequest.php

--- review/handel/handelReviewRequest.php
+++ review/handel/handelReviewRequest.php
@@ -1,31 +1,8 @@
 <?php
 
-//for test
-//echo "hallo";
-
 /* empty variables to carry form data */
 $behavior = $time = $cleaness = $material = $materialPrice = $tps = "";
 $workmanship = $workmanshipInp = $preview = $price = $knowUs = $rating = $replay = "";
-
-/* connect with database */
-/* variables to carry db info */
-/*
-$serverName = "localhost";
-$userName = "root";
-$dbPass = "";
-$dbName = "sanyaadelivery";
-
-//create connection
-$conn = new mysqli ($serverName , $userName , $dbPass , $dbName);
-
-//check connection
-if ($conn->connect_error) {
-    die ("<h3>فشل الإتصال مع قواعد البيانات</h3><br>".$conn->connect_error);
-}
-
-//to define arabic language
-mysqli_set_charset($conn,'utf8');
-*/
 
 $serverName = "den1.mysql6.gear.host";
 $userName = "sanyaatest";
@@ -40,10 +17,46 @@ if ($conn->connect_error) {
     die ("<h3>فشل الإتصال مع قواعد البيانات</h3><br>".$conn->connect_error);
 }
 
+//get request_id
+//$requestID = $_GET['request_id'];
+
+$requestID = 3;
+
 $selectQuery = "SELECT request_id
                 FROM follow_up_t";
 
 $result = $conn->query($selectQuery);
+
+
+//return request cost
+function getCost(int $requestID): double {
+    $costQuery = "SELECT product_cost
+                  FROM follow_up_t
+                  WHERE request_id = $requestID";
+    $requestCost = mysql_fetch_array($costQuery);
+    if(checkRequestIDIfUnsigned($requestID) == false){
+        return 0;
+    }
+    return $requestCost;
+}
+
+//update know us
+$knowUsQuery = "SELECT know_us
+FROM follow_up_t
+WHERE request_id = $requestID";
+$knowUs = $conn->query($knowUsQuery);
+
+function updateKnowUs(int $reqeustID, string $knowUs): bool {
+    if(checkRequestIDIfUnsigned($requestID) == false){
+        return false;
+    }
+    if($knowUs == NULL){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
 /* get form data */
 if (isset($_GET['behavior'])) {
@@ -56,10 +69,32 @@ if (isset($_GET['time'])) {
 
 if (isset($_GET['cleanness'])) {
     $cleaness = $_GET['cleanness'];
+    //mapping
+    function clean($cleanValue){
+        if($cleanValue == "نظف المكان"){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+        $cleanValueInput = array("نظف المكان" , "ساب المكان وحش");
+        array_map("clean" , $cleanValueInput);
+    }
 }
 
 if (isset($_GET['material'])) {
     $material = $_GET['material'];
+    //mapping
+    function bringMaterial($materialValue){
+        if($materialValue == "جاب خامات"){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+        $materialValueInput = array("جاب خامات" , "ماجابش خامات");
+        array_map("bringMaterial" , $materialValueInput);
+    }
 }
 
 if (isset($_GET['materialPrice'])) {
@@ -68,6 +103,17 @@ if (isset($_GET['materialPrice'])) {
 
 if (isset($_GET['tps'])) {
     $tps = $_GET['tps'];
+    //mapping
+    function takeTps($tpsValue){
+        if($tpsValue == "أخد تبس"){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+        $tpsValueInput = array("أخد تبس" , "ما أخدش تبس");
+        array_map("takeTps" , $tpsValueInput);
+    }
 }
 
 if (isset($_GET['workmanshipInp'])) {
@@ -97,11 +143,6 @@ if (isset($_GET['rating'])) {
 if (isset($_GET['replay'])) {
     $replay = $_GET['replay'];
 }
-
-// for test
-// echo $behavior."<br>".$time."<br>".$cleaness."<br>".$material."<br>".$materialPrice."<br>".$tps."<br>";
-// echo $workmanship."<br>".$workmanshipInp."<br>".$preview."<br>".$price."<br>".$knowUs."<br>";
-// echo $rating."<br>".$replay."<br>";
 
 //insert data into db
 $insertQuery = "INSERT INTO follow_up_t
